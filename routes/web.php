9d025c3fa9d3f9b6c1cd46051bd94e41b11be560
unknown <luccasdrapala@gmail.com>
2022-08-12 19:53:24 -0300

    construindo rotas teste

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hello world Laravel !!!";
 });
+
+Route::get('/sobre-nos', function () {
+    return "Sobre nós !!!";
+});
+
+Route::get('/contato', function () {
+    return "Contato";
+});
+
+Route::get($uri, $callback);
