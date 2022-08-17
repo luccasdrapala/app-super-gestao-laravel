3a5cecf33b7b62d9e788bda1c71b21d6bcbd6c53
unknown <luccasdrapala@gmail.com>
2022-08-17 20:16:21 -0300

    usando ->where para especificar os tipo de parametros

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/contato/{nome}/{numero}/{opcional?}', function(string $nome, int $numero, string $mensagem = 'Mensagem não informada') {
-    echo 'Estamos aqui !' . $nome . ' Numero: ' . $numero;
-    echo '<br>';
-    echo 'parametro opcional: ' . $mensagem;
-});
+Route::get('/contato/{nome}/{numero_id}', 
+    function(
+        string $nome, 
+        int $numero_id = 1
+    ) {
+        echo 'Estamos aqui !<br>' . $nome . ' Numero: ' . $numero_id;
+    }
+)->where('numero_id','[0-9]+')->where('nome', '[A-Za-z]+');
