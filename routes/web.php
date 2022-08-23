8d0143e5cb0bf5e2690af7016c51fd84c3cfb6ba
unknown <luccasdrapala@gmail.com>
2022-08-23 20:26:25 -0300

    redirecionando rota com Route::redirect()

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,21 @@ Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::get('/login', function () {return 'login';})->name('site.login');
 
 // ROTAS AGRUPADAS EM /app
-route::prefix('/app')->group(
+Route::prefix('/app')->group(
     function () {
-        Route::get('/clientes', function () {return 'clientes';});
-        Route::get('/fornecedores', function () {return 'fornecedores';});
-        Route::get('/produtos', function () {return 'produtos';});
+        Route::get('/clientes', function () {return 'clientes';})->name('app.clientes');
+        Route::get('/fornecedores', function () {return 'fornecedores';})->name('app.fornecedores');
+        Route::get('/produtos', function () {return 'produtos';})->name('app.produtos');
     }
 );
+
+Route::get(('/rota1'), function() {
+    echo 'Rota 1';
+})->name('site.rota1');
+
+// Route::get(('/rota2'), function() {
+//     echo 'Rota 2';
+// })->name('site.rota2');
+
+Route::redirect(('/rota2'), ('/rota1'));
 
