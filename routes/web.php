332f1c98dd6e8d0cc9cee104c849af827c14a501
unknown <luccasdrapala@gmail.com>
2022-08-23 20:31:27 -0300

    redirecionando rotas com funcao de callback

--- routes/web.php
+++ routes/web.php
@@ -31,9 +31,9 @@ Route::get(('/rota1'), function() {
     echo 'Rota 1';
 })->name('site.rota1');
 
-// Route::get(('/rota2'), function() {
-//     echo 'Rota 2';
-// })->name('site.rota2');
+Route::get(('/rota2'), function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
 
-Route::redirect(('/rota2'), ('/rota1'));
+//Route::redirect(('/rota2'), ('/rota1'));
 
