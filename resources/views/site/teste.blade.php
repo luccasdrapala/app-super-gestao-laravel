9f5166334cf5b186565ad39ad69df984f7b47001
unknown <luccasdrapala@gmail.com>
2022-08-30 20:51:42 -0300

    Recebendo parametros do controller na view

---
+++ resources/views/site/teste.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Teste</title>
+</head>
+<body>
+    <h3>Essa é a pagina teste</h3>
+
+    <!-- <h4>Recebendo parametros do controler: metodo array associativo</h4>
+    <p>P1 === {{ $p1 }}</p>
+    <p>P2 === {{ $p2 }}</p>
+    <hr> -->
+
+    <!-- <h4>Recebendo parametros do controler: metodo compact</h4>
+    <p>P1 === {{ $p1 }}</p>
+    <p>P2 === {{ $p2 }}</p> -->
+    <hr>
+
+    <h4>Recebendo parametros do controler: metodo with</h4>
+    <p>P1 === {{ $p1 }}</p>
+    <p>P2 === {{ $p2 }}</p>
+
+</body>
+</html>
