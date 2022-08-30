8096b05e46dacdd394a5d61eafbb8d5718f248c7
unknown <luccasdrapala@gmail.com>
2022-08-29 21:32:07 -0300

    passando parametros da rota para o controller

--- routes/web.php
+++ routes/web.php
@@ -27,15 +27,7 @@ Route::prefix('/app')->group(
     }
 );
 
-Route::get(('/rota1'), function() {
-    echo 'Rota 1';
-})->name('site.rota1');
-
-Route::get(('/rota2'), function() {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
-
-//Route::redirect(('/rota2'), ('/rota1'));
+Route::get(('/teste/{p1}/{p2}'), 'TesteController@teste')->name('teste');
 
 Route::fallback(function() {
     echo 'pagina não encontrada ';
