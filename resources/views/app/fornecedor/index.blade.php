d1d52c5911f2c217ba1465e2597230d976b902d5
unknown <luccasdrapala@gmail.com>
2022-09-01 21:21:36 -0300

    endendo a sintaxe do blade

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,3 +1,18 @@
 <h3>Página index (Fornecedor)</h3>
 
 {{--Comentario do blade--}}
+
+@php
+    
+
+
+@endphp
+
+{{-- @dd($fornecedores) Lista array na tela--}}
+
+
+@if (count($fornecedores)> 0 && count($fornecedores)<10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+@elseif (count($fornecedores)>10)
+    <h3>Existem mais de 10</h3>
+@endif
